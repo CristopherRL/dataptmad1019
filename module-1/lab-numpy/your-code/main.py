b0518dd9a7c5e32abfe939855291c8cd33eca4ec
CristopherRL <Cristopher.rojas.lepe@gmail.com>
2019-11-17 15:24:23 +0100

    main.py uploaded

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -31,86 +31,118 @@ if a.size == b.size:
 
 
 #8. Are you able to add a and b? Why or why not?
-c = a+b
-print(c)
+try:
+    c = a+b
+    print(c)
+except:
+    print("It's not possible to add 2 different arrays with different dimensions")
+
 #these arrays don't have the same dimensions
 
-#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+c = b.transpose(1,2,0)
+#d = np.array(a).reshape(2,3,5) > it's not the same
+print(c)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = a + c
+#Because they have the same dimension
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
+print (a)
+print (d)
 
 #12. Multiply a and c. Assign the result to e.
 
+e = a*c
 
+#in this operation evary element is multyplied for 1 
 
 #13. Does e equal to a? Why or why not?
-
+if np.array_equal(e,a):
+    print("e and a are equal")
+else:
+    print("e and a are NOT equal")
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
-
+d_max = d.max()
+print(d_max)
+d_min = d.min()
+print(d_min)
+d_mean = d.mean()
+print(d_mean)
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
+f = np.empty([2,3,5])
+print(f)
 
 
 
 
-"""
+
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
-If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
-If a value equals to d_mean, assign 50 to the corresponding value in f.
-Assign 0 to the corresponding value(s) in f for d_min in d.
-Assign 100 to the corresponding value(s) in f for d_max in d.
-In the end, f should have only the following values: 0, 25, 50, 75, and 100.
-Note: you don't have to use Numpy in this question.
-"""
+#If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
+#If a value equals to d_mean, assign 50 to the corresponding value in f.
+#Assign 0 to the corresponding value(s) in f for d_min in d.
+#Assign 100 to the corresponding value(s) in f for d_max in d.
+#In the end, f should have only the following values: 0, 25, 50, 75, and 100.
+#Note: you don't have to use Numpy in this question.
 
+f_list=[]
+for x in d:
+    #print(x)
+    for y in x:
+        for z in y:
+            #print ('-',z)
+            if z>d_min and z<d_mean:
+                f_list.append(25)
+            elif z>d_mean and z<d_max:
+                f_list.append(75)
+            elif z==d_mean:
+                f_list.append(50)
+            elif z==d_min:
+                f_list.append(0)
+            elif z==d_max:
+                f_list.append(100)
+                
+f = np.array(f_list).reshape(2,3,5)
+#print(f)
 
-
-
-"""
 #17. Print d and f. Do you have your expected f?
-For instance, if your d is:
-array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
-        [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
-        [1.72201435, 1.1862918 , 1.87078449, 1.7726778 , 1.88180042]],
+#For instance, if your d is:
+#array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
+#        [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
+#        [1.72201435, 1.1862918 , 1.87078449, 1.7726778 , 1.88180042]],
 
-       [[1.44747908, 1.31673383, 1.02000951, 1.52218947, 1.97066381],
-        [1.79129243, 1.74983003, 1.96028037, 1.85166831, 1.65450881],
-        [1.18068344, 1.9587381 , 1.00656599, 1.93402165, 1.73514584]]])
+#       [[1.44747908, 1.31673383, 1.02000951, 1.52218947, 1.97066381],
+#        [1.79129243, 1.74983003, 1.96028037, 1.85166831, 1.65450881],
+#        [1.18068344, 1.9587381 , 1.00656599, 1.93402165, 1.73514584]]])
 
-Your f should be:
-array([[[ 75.,  75.,  75.,  25.,  75.],
-        [ 75.,  75.,  25.,  25.,  25.],
-        [ 75.,  25.,  75.,  75.,  75.]],
+#Your f should be:
+#array([[[ 75.,  75.,  75.,  25.,  75.],
+#        [ 75.,  75.,  25.,  25.,  25.],
+#        [ 75.,  25.,  75.,  75.,  75.]],
 
-       [[ 25.,  25.,  25.,  25., 100.],
-        [ 75.,  75.,  75.,  75.,  75.],
-        [ 25.,  75.,   0.,  75.,  75.]]])
-"""
+#       [[ 25.,  25.,  25.,  25., 100.],
+#        [ 75.,  75.,  75.,  75.,  75.],
+#        [ 25.,  75.,   0.,  75.,  75.]]])
+#
 
 
-"""
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
-("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
-array([[[ 'D',  'D',  'D',  'B',  'D'],
-        [ 'D',  'D',  'B',  'B',  'B'],
-        [ 'D',  'B',  'D',  'D',  'D']],
+#("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
+#array([[[ 'D',  'D',  'D',  'B',  'D'],
+#        [ 'D',  'D',  'B',  'B',  'B'],
+#        [ 'D',  'B',  'D',  'D',  'D']],
 
-       [[ 'B',  'B',  'B',  'B',  'E'],
-        [ 'D',  'D',  'D',  'D',  'D'],
-        [ 'B',  'D',   'A',  'D', 'D']]])
-Again, you don't need Numpy in this question.
-"""
+#       [[ 'B',  'B',  'B',  'B',  'E'],
+#        [ 'D',  'D',  'D',  'D',  'D'],
+#        [ 'B',  'D',   'A',  'D', 'D']]])
+#Again, you don't need Numpy in this question.
